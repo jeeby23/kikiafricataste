f802a65c6ecda9a70e2a0d58332d9b841164c703
Alabi Fathiu <alabiabolore4@gmail.com>
2026-06-27 19:18:58 +0100

    fix: remove duplicate status declaration and cleanup imports

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["@prisma/client"],
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+  },
 };
 
 export default nextConfig;
